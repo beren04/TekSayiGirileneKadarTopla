565e1941a3e6dd746165600a34dcc0399b40fb94
Beren <111902297+beren04@users.noreply.github.com>
2022-09-25 11:26:51 +0300

    Update and rename README.md to kod.java

---
+++ kod.java
@@ -0,0 +1,19 @@
+package first;
+
+import java.util.Scanner;
+
+public class BelliBirSayiGirileneKadarTopla {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        int total = 0;
+        do {
+            System.out.print("Enter a number:");
+            number = input.nextInt();
+            if (number % 2 == 0 && number % 4 == 0) {
+                total += number;
+            }
+        }while (number % 2 == 0) ;
+        System.out.print("Total:" + total);
+    }
+}
